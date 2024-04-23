e7ce41f5609ad91471b575db3a3c018313d0184a
Val <saviorand@gmail.com>
2024-04-23 20:44:57 +0200

    Quartz sync: Apr 23, 2024, 8:44 PM

---
+++ content/examples/modesOfInference/ruleCase.pl
@@ -0,0 +1,57 @@
+:- use_module(library(scasp)).
+:- ensure_loaded('../../functions/inference.pl').
+
+% ontology(Ontology, Predicates) :-
+%     Ontology = [
+%         sack(s1),
+%         bean(b1),
+%         white(Y)
+%     ],
+%     Predicates = [sack, bean, from_s1].
+
+% theory(Rule, Case, Result) :-
+%     Case = [
+%         from_s1(b1), % These beans are from this sack
+%         positive(b1)
+%         ],
+%     Result = white. % These beans are white 
+
+% ?- ontology(Ontology, Predicates), theory([], Case, Result), inference(induction, Ontology, Predicates, [], Case, Result).
+
+% ontology(Ontology, Predicates) :-
+%     Ontology = [
+%         sack(s1),
+%         bean(X),
+%         white(Y),
+%         from(X,Z)
+%     ].
+
+% theory(Rule, Case, Result) :-
+%     Rule = [
+%         % All beans from this sack are white
+%         bean(B),
+%         white(B) :- from(B,s1)
+%         ],
+%     Case = [
+%         from(b1,s1) % These beans are from this sack
+%         ],
+%     Result = white(X). % These beans are white 
+
+% ?- ontology(Ontology, []), theory(Rule, Case, Result), inference(deduction, Ontology, [], Rule, Case, Result).
+
+
+ontology(Ontology, Predicates) :-
+    Ontology = [].
+
+theory(Rule, Case, Result) :-
+    Rule = [
+        white(X) :- from(X,s1) % All beans from this sack are white
+        ],
+    Case = [],
+    Result = white(b1). % These beans are white 
+
+% Defined on the top level
+#abducible from(X,Z).
+
+?- ontology(Ontology, []), theory(Rule, Case, Result), inference(abduction, Ontology, [], Rule, Case, Result).
+
--- content/functions/inference.pl
+++ content/functions/inference.pl
@@ -1,16 +1,26 @@
 :- use_module(library(scasp)).
+:- use_module(library(scasp/human)).
 :- ensure_loaded('./functions/createBackground.pl').
 :- ensure_loaded('./First-Order-Learner-of-Default/fold.pl').
+:- style_check(-singleton).
 
 inference(Type, Ontology, Predicates, Rule, Case, Result) :-
     append(Ontology, Rule, OR),
     append(OR, Case, ORC),
     create_background(ORC),
     extract_pos_neg(ORC, Positive, Negative),
-    (Type = deduction -> scasp(Result, [model(M), tree(T)]);
-    Type = abduction -> scasp(Result, [model(M), tree(T)]);
+    (Type = deduction -> scasp(Result, [model(M), tree(T)]), print_justification(M, T);
+    Type = abduction -> scasp(Result, [model(M), tree(T)]), print_justification(M, T);
     Type = induction -> fold(Result, Positive, Negative, ORC, Predicates, D, AB, false)).
 
 extract_pos_neg(Background, Positive, Negative) :-
     findall(X, member(positive(X), Background), Positive),
     findall(X, member(negative(X), Background), Negative).
+
+print_justification(Model, Tree) :-
+    writeln('Model:'),
+    with_output_to(string(MOut), human_model(Model, [])),
+    writeln(MOut),
+    with_output_to(string(TOut), human_justification_tree(Tree, [])),
+    writeln('Justification Tree:'),
+    writeln(TOut).
